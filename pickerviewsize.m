1f8025950e87a2931010a737387b2df3c28d76d8
xiaohuahao <1169756883@qq.com>
2015-07-21 00:48:16 +0800

    custom the pickerView size
    custom the pickerView size

---
+++ pickerviewsize.m
@@ -0,0 +1,25 @@
+// PickerViewSize
+// show the pickerView size
+//
+// IDECodeSnippetCompletionPrefix: ps
+// IDECodeSnippetCompletionScopes: [CodeBlock]
+// IDECodeSnippetIdentifier: 9325344F-0AE5-4B88-8B5E-FCA5297082FC
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 0
+CGFloat color[4] = {1,1,1,1};
+    
+    CGColorSpaceRef rgb = CGColorSpaceCreateDeviceRGB();
+    CGColorRef layerBounderColor = CGColorCreate(rgb, color);
+    CGColorSpaceRelease(rgb);
+    
+    
+    self.pickerView = [[UIPickerView alloc] initWithFrame:CGRectZero];
+    
+    CALayer *viewLayer = self.pickerView.layer;
+    [viewLayer setBounds:CGRectMake(0, 0, 100, 100)];
+    [viewLayer setBackgroundColor:layerBounderColor];
+    [viewLayer setContentsRect:CGRectMake(0, 0, 10, 10)];
+    [viewLayer setBorderWidth:20];
+    [viewLayer setBorderColor:layerBounderColor];
+    CGColorRelease(layerBounderColor);
