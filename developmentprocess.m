fe7358ed3d7852df7b47db3e75495ae0df69fafd
xiaohuahao <1169756883@qq.com>
2015-07-20 21:31:48 +0800

    DevelopmentProcess
    development process and header

---
+++ developmentprocess.m
@@ -0,0 +1,41 @@
+// DevelopmentProcess
+// development process and header
+//
+// IDECodeSnippetCompletionPrefix: dp
+// IDECodeSnippetCompletionScopes: [Preprocessor]
+// IDECodeSnippetIdentifier: CFA6ED47-862A-44E6-A4DA-E4F34160DFEE
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C-Plus-Plus
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 0
+//procjectName > storyBoard > thirdLibrary > tool(header and manager) > model > 界面
+
+
+
+// other
+typedef NS_ENUM(NSInteger,<#ButtonTypeName#>) {
+    
+    <#ADDBUTTONTYPE#> = 1001,
+    <#CHOOSEBUTTONTYPE#>,
+    <#BACKNUTTONTYPE#>,
+    <#LISTBUTTONTYPE#>,
+};
+
+#define buttonTypeName tag
+
+#define SCREENSIZE [[UIScreen mainScreen] bounds].size
+// URL
+#define DALING_PAGE_URL @"http://m.api.zhe800.com/v4/deals?page=1&per_page=80&user_type=1&user_role=1"
+
+@class MyNavButtonClickDelegate,SKYTabBarController;
+// sharesdk
+
+
+// TOOL
+#import "HttpManager.h"
+#import "AFNetworking.h"
+#import "SKYTabBarController.h"
+
+// CLASS
+
+#import "DaLingViewController.h"
+#import "ClassifyViewController.h"
